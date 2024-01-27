bea8a568942211019e1cc34eb5ecbbf371752430
Lemuel Gempesao <lemuelgempesao40@gmail.com>
2024-01-27 11:07:34 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.getElementById('burger');
+    const menu = document.querySelector('.menu');
+    const menuLinks = document.querySelectorAll('.menu a');
+
+    burger.addEventListener('click', function () {
+        menu.classList.toggle('show');
+    });
+
+    // Add an event listener for each menu link
+    menuLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            // Hide the menu after clicking a link
+            menu.classList.remove('show');
+        });
+    });
+});
+
